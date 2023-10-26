4d6ceffa4e8a8bfa7dafbdb1a53ce7ba613de3dd
天宇 胡 <2524183995@qq.com>
2023-10-26 19:32:29 +0800

    添加一个空白窗口

--- main.cpp
+++ main.cpp
@@ -1,1 +1,56 @@
-#include <iostream>
+#include<Windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)//回调函数
+{
+	switch (message)
+	{
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	WNDCLASSEX windowClass = { 0 };
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = WindowProc;//设置回调函数的地方
+	windowClass.hInstance = hInstance;
+	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	windowClass.lpszClassName = L"RenderClass";
+
+	RegisterClassEx(&windowClass);
+
+	HWND hwnd = CreateWindow(
+		windowClass.lpszClassName,
+		L"Render",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		800,
+		600,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr);
+
+	ShowWindow(hwnd, SW_SHOW);
+
+
+	MSG msg = {};
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+
+	return 0;
+}
